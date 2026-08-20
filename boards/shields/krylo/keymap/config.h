febaa495825de7c57f6c058706de9019b869acd4
rawrex <rawrex@proton.me>
2026-08-20 19:52:19 +0700

    Miryoku features internalized in this repo, now when we build ZMK we dont need to provide the miryoku_zmk additionally and all the edits to the original miryoku_zmk in the context of the krylo are now also internalized here

---
+++ boards/shields/krylo/keymap/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// #define KRYLO_KLUDGE_DOUBLETAPBOOT        // Double-tap bootloader guard
+// #define KRYLO_KLUDGE_SOFT_OFF             // Soft off (uses CONFIG_ZMK_SLEEP)
+// #define KRYLO_KLUDGE_TOPROWCOMBOS         // Top-row combos
+// #define KRYLO_KLUDGE_BOTTOMROWCOMBOS      // Bottom-row combos
+// #define KRYLO_KLUDGE_THUMBCOMBOS          // Thumb combos
+// #define KRYLO_CLIPBOARD_FUN               // Clipboard via media keys
+// #define KRYLO_CLIPBOARD_MAC               // Clipboard via Cmd+Z/C/V/X
+// #define KRYLO_CLIPBOARD_WIN               // Clipboard via Ctrl+Z/C/V/X
---
+++ boards/shields/krylo/keymap/macros.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "config.h"
+
+/* --------------------------------------------------------------------------
+ * Layer numbers
+ * -------------------------------------------------------------------------- */
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MEDIA  5
+#define U_NUM    6
+#define U_SYM    7
+#define U_FUN    8
+
+/* --------------------------------------------------------------------------
+ * Cyrillic key aliases (QWERTY keycodes, as in miryoku keys_ru.h)
+ * -------------------------------------------------------------------------- */
+
+#define RU_J  Q      // Й
+#define RU_TS W      // Ц
+#define RU_U  E      // У
+#define RU_K  R      // К
+#define RU_YE T      // Е
+#define RU_N  Y      // Н
+#define RU_G  U      // Г
+#define RU_SH I      // Ш
+#define RU_SHCH O    // Щ
+#define RU_Z  P      // З
+#define RU_KH LBKT   // Х
+#define RU_HARD RBKT // Ъ
+
+#define RU_F  A      // Ф
+#define RU_Y  S      // Ы
+#define RU_V  D      // В
+#define RU_A  F      // А
+#define RU_P  G      // П
+#define RU_R  H      // Р
+#define RU_O  J      // О
+#define RU_L  K      // Л
+#define RU_D  L      // Д
+#define RU_ZH SEMI   // Ж
+#define RU_E  SQT    // Э
+
+#define RU_YA Z      // Я
+#define RU_CH X      // Ч
+#define RU_S  C      // С
+#define RU_M  V      // М
+#define RU_I  B      // И
+#define RU_T  N      // Т
+#define RU_SOFT M    // Ь
+#define RU_B  COMMA  // Б
+#define RU_YU DOT    // Ю
+
+// "ё" symbol is not used
+
+/* --------------------------------------------------------------------------
+ * Shared macros
+ * -------------------------------------------------------------------------- */
+
+#define U_TAPPING_TERM 200
+
+#define U_NA &none // present but not available for use
+#define U_NP &none // key is not present
+#define U_NU &none // available but not used
+
+// Bootloader / soft off (miryoku: MIRYOKU_KLUDGE_DOUBLETAPBOOT + SOFT_OFF)
+#if defined(KRYLO_KLUDGE_DOUBLETAPBOOT)
+  #if defined(KRYLO_KLUDGE_SOFT_OFF)
+    #define U_BOOT &u_soft_off
+  #else
+    #define U_BOOT &u_bootloader
+  #endif
+#else
+  #if defined(KRYLO_KLUDGE_SOFT_OFF)
+    #define U_BOOT &soft_off
+  #else
+    #define U_BOOT &bootloader
+  #endif
+#endif
+
+// Clipboard (miryoku_clipboard.h)
+#if defined(KRYLO_CLIPBOARD_FUN)
+  #define U_RDO &kp K_AGAIN
+  #define U_PST &kp K_PASTE
+  #define U_CPY &kp K_COPY
+  #define U_CUT &kp K_CUT
+  #define U_UND &kp K_UNDO
+#elif defined(KRYLO_CLIPBOARD_MAC)
+  #define U_RDO &kp LS(LG(Z))
+  #define U_PST &kp LG(V)
+  #define U_CPY &kp LG(C)
+  #define U_CUT &kp LG(X)
+  #define U_UND &kp LG(Z)
+#elif defined(KRYLO_CLIPBOARD_WIN)
+  #define U_RDO &kp LC(Y)
+  #define U_PST &kp LC(V)
+  #define U_CPY &kp LC(C)
+  #define U_CUT &kp LC(X)
+  #define U_UND &kp LC(Z)
+#else
+  #define U_RDO &kp K_AGAIN
+  #define U_PST &kp LS(INS)
+  #define U_CPY &kp LC(INS)
+  #define U_CUT &kp LS(DEL)
+  #define U_UND &kp K_UNDO
+#endif
