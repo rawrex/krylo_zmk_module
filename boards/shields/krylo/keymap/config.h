9b18519a707220dd8cc1c69d62a12f3633cedc6d
rawrex <rawrex@proton.me>
2026-08-29 19:20:51 +0700

    Combos are made use of, added the missing K, V, ESC, DEL as the appropriate combos

--- boards/shields/krylo/keymap/config.h
+++ boards/shields/krylo/keymap/config.h
@@ -1,8 +1,5 @@
 #pragma once
 
-// #define KRYLO_KLUDGE_TOPROWCOMBOS         // Top-row combos
-// #define KRYLO_KLUDGE_BOTTOMROWCOMBOS      // Bottom-row combos
-// #define KRYLO_KLUDGE_THUMBCOMBOS          // Thumb combos
 // #define KRYLO_CLIPBOARD_FUN               // Clipboard via media keys
 // #define KRYLO_CLIPBOARD_MAC               // Clipboard via Cmd+Z/C/V/X
 // #define KRYLO_CLIPBOARD_WIN               // Clipboard via Ctrl+Z/C/V/X
