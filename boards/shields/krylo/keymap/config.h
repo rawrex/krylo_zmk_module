14e96a5a2040b963d2f12c1bb69918feea91c31e
rawrex <rawrex@proton.me>
2026-08-30 18:22:18 +0700

    Whitespace clean up

--- boards/shields/krylo/keymap/config.h
+++ boards/shields/krylo/keymap/config.h
@@ -1,8 +1,8 @@
 #pragma once
 
-// #define KRYLO_CLIPBOARD_FUN               // Clipboard via media keys
-// #define KRYLO_CLIPBOARD_MAC               // Clipboard via Cmd+Z/C/V/X
-// #define KRYLO_CLIPBOARD_WIN               // Clipboard via Ctrl+Z/C/V/X
+// #define KRYLO_CLIPBOARD_FUN // Clipboard via media keys
+// #define KRYLO_CLIPBOARD_MAC // Clipboard via Cmd+Z/C/V/X
+// #define KRYLO_CLIPBOARD_WIN // Clipboard via Ctrl+Z/C/V/X
 
 #define KRYLO_BT_HOP_ENABLE
 #define KRYLO_BT_HOP_PROFILE 4
